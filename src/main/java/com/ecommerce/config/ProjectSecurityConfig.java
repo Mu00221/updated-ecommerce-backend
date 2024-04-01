3e5ef181ee03d2e00b688322b5521bd96f686120
Mu00221 <blacklives864@gmail.com>
2024-03-31 22:35:07 -0500

    final cha

--- src/main/java/com/ecommerce/config/ProjectSecurityConfig.java
+++ src/main/java/com/ecommerce/config/ProjectSecurityConfig.java
@@ -39,10 +39,10 @@ public class ProjectSecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Arrays.asList("https://e-backend-production.up.railway.app/login", 
+                        config.setAllowedOrigins(Arrays.asList( 
                                                                 "https://afghan-couture.vercel.app",
                                                                 "http://localhost:4200",
-                                                                "http://localhost:4000"));
+                                                                "http://localhost:3000"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
