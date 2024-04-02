826e4b2db549a7c0218b68716d1966b65fbccb8d
Mu00221 <blacklives864@gmail.com>
2024-04-02 13:10:50 -0500

    Nice

--- src/main/java/com/ecommerce/controller/UserProductController.java
+++ src/main/java/com/ecommerce/controller/UserProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.ecommerce.service.ProductService;
 import com.ecommerce.user.domain.ProductSubCategory;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api")
 public class UserProductController {
 	
